434135060b425d6dbb9bafa82f9f196264f8be28
shankuljain <shankuljain@Shankuls-MacBook-Air.local>
2015-02-18 17:57:25 +0530

    Initial Commit

---
+++ generic stack/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  generic stack
+//
+//  Created by Shankul Jain on 18/02/15.
+//  Copyright (c) 2015 Shankul Jain. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
